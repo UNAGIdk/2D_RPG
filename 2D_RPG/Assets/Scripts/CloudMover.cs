24490d5fe00ac01fdc33c69016d8fbc18773a371
Denis <79550292+UNAGIdk@users.noreply.github.com>
2022-09-22 03:25:37 +1000

    Полностью сделана механика движущихся облаков

--- 2D_RPG/Assets/Scripts/CloudMover.cs
+++ 2D_RPG/Assets/Scripts/CloudMover.cs
@@ -8,10 +8,13 @@ public class CloudMover : MonoBehaviour
     private void Start()
     {
         moveDelta = new Vector3(xSpeed, 0f, 0f);
+        transform.localPosition = new Vector3(transform.localPosition.x, 0, transform.localPosition.z);
     }
 
     void Update()
     {
-        transform.Translate(moveDelta.x, 0, 0); // * Time.deltaTime
+        transform.Translate(moveDelta.x * Time.deltaTime, 0, 0); // * Time.deltaTime
+        if(transform.localPosition.x > 50.0f)
+            Destroy(gameObject);
     }
 }
